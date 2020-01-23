0bcfc4902a048b38872854140ba18870e14108a8
Budi Arsana <gusdecool@gmail.com>
2020-01-24 02:18:17 +0800

    Finished preprocessed data

--- Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template_budi.py
+++ Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template_budi.py
@@ -10,13 +10,28 @@ dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# # Take care of missing data
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values=np.nan)
+# Take care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values="NaN")
 imputer = imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 
-# # Encoding categorical data
-from sklearn.preprocessing import LabelEncoder
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_x = LabelEncoder()
 x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+x = oneHotEncoder.fit_transform(x).toarray()
+
+labelencored_y = LabelEncoder()
+y = labelencoder_x.fit_transform(y)
+
+# Splitting the dataset into training set
+from sklearn.model_selection import train_test_split
+x_test, x_train, y_test, y_train = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+# Feature scalling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
