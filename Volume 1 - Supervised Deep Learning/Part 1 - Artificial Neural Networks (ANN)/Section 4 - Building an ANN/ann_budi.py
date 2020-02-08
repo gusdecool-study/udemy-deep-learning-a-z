15ddbfbe7d8e57ffb61dd1767889313c546b5ec8
Budi Arsana <gusdecool@gmail.com>
2020-02-08 12:53:56 +0800

    init

---
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann_budi.py
@@ -0,0 +1,70 @@
+# Artificial Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# -----------------------------------------------------------------------------
+# Encoding categorical data
+# -----------------------------------------------------------------------------
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+# Encode country
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+
+# Encode gender
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+
+#ct = ColumnTransformer([('Country', OneHotEncoder(categories='auto'))])
+#onehotencoder = OneHotEncoder()
+#X = onehotencoder.fit_transform(X).toarray()
+#X = X[:, 1:]
+
+ct = ColumnTransformer(
+    [('Country', OneHotEncoder(categories='auto'), [0])], 
+    remainder='passthrough')
+
+X = ct.fit_transform(X)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Feature scalling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler(with_mean=False)
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Fitting logistic refresstion to the training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the result
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
