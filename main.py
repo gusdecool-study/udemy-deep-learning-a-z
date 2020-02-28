a02cff0f8c90c18c3d7f395d4e8387cc5a6a3097
Budi Arsana <gusdecool@gmail.com>
2020-02-28 23:19:29 +0800

    finished refactoring pre-processing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from sklearn.metrics import confusion_matrix
 from s04_building_ann.cx01_data_processing import load_dataset, encode, split, scaling
+from s04_building_ann.cx02_ann import Ann
 
 # -------------------------------------------------------------------------------------------------
 # data pre-processing
@@ -10,3 +12,21 @@ x = encode(x, 2)  # encode gender
 
 x_train, x_test, y_train, y_test = split(x, y)  # split data for test and train
 x_train, x_test = scaling(x_train, x_test)  # scaling the data
+
+# -------------------------------------------------------------------------------------------------
+# Artificial Neural Networks (ANN)
+# -------------------------------------------------------------------------------------------------
+
+ann = Ann()
+ann.add_layer(6)  # first hidden layer
+ann.add_layer(6)  # second hidden layer
+ann.add_layer(1, 'sigmoid')  # output layer, 1 output using Sigmoid
+ann.compile()  # configure the model for training
+ann.train(x_train, y_train)  # train the model
+
+y_prediction = ann.predict(x_test)
+y_prediction = (y_prediction > 0.5)  # Change prediction to Boolean
+
+cm = confusion_matrix(y_test, y_prediction)  # Making the confusion matrix
+
+# Examine the var cm above
---
+++ s04_building_ann/cx02_ann.py
@@ -0,0 +1,51 @@
+from keras import Sequential
+from keras.layers import Dense
+
+
+class Ann:
+    """
+    Artificial Neural Networks (ANN) with Sequential classifier
+    """
+
+    __classifier = Sequential()
+
+    def add_layer(self, node, activation='relu'):
+        """
+        add ANN layer
+
+        :type activation: str
+        :type node: int
+        :param node: How may nodes will be creating
+        :param activation: activation name, default 'relu'
+        """
+
+        self.__classifier.add(Dense(node, kernel_initializer='uniform', activation=activation))
+
+    def compile(self):
+        """
+        Configures the model for training.
+        """
+
+        self.__classifier.compile('adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    def train(self, x, y):
+        """
+        Train the data set
+
+        :type y: numpy.ndarray
+        :type x: numpy.ndarray
+        :param x: x data
+        :param y: y data
+        """
+
+        self.__classifier.fit(x, y, batch_size=10, epochs=30)
+
+    def predict(self, x):
+        """
+        predict the independent variables
+
+        :type x: numpy.ndarray
+        :param x: x data to test
+        :return: return list of prediction result
+        :rtype: numpy.ndarray
+        """
