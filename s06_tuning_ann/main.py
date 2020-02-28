630262d4b28f4620592c2b55e5323f6e84342cfe
Budi Arsana <gusdecool@gmail.com>
2020-02-29 01:10:35 +0800

    Tuning the classifier

--- s06_tuning_ann/main.py
+++ s06_tuning_ann/main.py
@@ -1,9 +1,11 @@
 from keras import Sequential
 from keras.wrappers.scikit_learn import KerasClassifier
-
+from sklearn.model_selection import cross_val_score
 from s04_building_ann.ann import Ann
+from s04_building_ann.main_data_pre_processing import x_train, y_train
 
 
+# Evaluating the ANN
 def build_classifier():
     """
     build classifier
@@ -21,4 +23,7 @@ def build_classifier():
     return ann.get_classifier()
 
 
-classifier = KerasClassifier(build_classifier, batch_size=10, nb_epoch=100)
+classifier = KerasClassifier(build_classifier, batch_size=10, nb_epoch=30)
+accuracies = cross_val_score(classifier, x_train, y_train, cv=10, n_jobs=-1)
+mean = accuracies.mean()
+variance = accuracies.std()
